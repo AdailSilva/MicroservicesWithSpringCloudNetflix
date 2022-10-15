97c198ee02503f9ec9b77a5367a0de0d381da6cb
AdailSilva <adailsilva101@hotmail.com>
2022-10-15 16:49:55 -0300

    Project ms-payroll created

---
+++ ms-payholl/src/main/java/com/adailsilva/mspayroll/MsPayhollApplication.java
@@ -0,0 +1,13 @@
+package com.adailsilva.mspayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPayhollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPayhollApplication.class, args);
+	}
+
+}
---
+++ ms-payholl/src/test/java/com/adailsilva/mspayroll/MsPayhollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adailsilva.mspayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPayhollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
